2e11d436704e47e2086484710d808c79ab1509e3
Subham Mani <manisubham09@gmail.com>
2023-09-14 18:15:50 +0530

    add task btn and modal

--- src/ui-main.js
+++ src/ui-main.js
@@ -1,8 +1,12 @@
 import createNav from "./nav";
 import { toggleHam } from "./nav";
+import createTasksUI from "./add-tasks-ui";
+import createModal from "./add-task-modal";
 
 export default function createUI(){
     const content = document.getElementById('content');
     content.appendChild(createNav());
+    content.appendChild(createTasksUI());
+    content.appendChild(createModal());
     toggleHam();
 }
