9fac5a10b5dac35b2d15bf77bcfaceee121f7261
Subham Mani <manisubham09@gmail.com>
2023-09-15 20:26:47 +0530

    load tasks on startup

--- src/ui-main.js
+++ src/ui-main.js
@@ -2,11 +2,15 @@ import createNav from "./nav";
 import { toggleHam } from "./nav";
 import createTasksUI from "./add-tasks-ui";
 import createModal from "./add-task-modal";
+import createTaskContainer from "./task-container";
+import loadTasks from "./load-tasks";
 
 export default function createUI(){
     const content = document.getElementById('content');
     content.appendChild(createNav());
     content.appendChild(createTasksUI());
     content.appendChild(createModal());
+    content.appendChild(createTaskContainer());
+    loadTasks();
     toggleHam();
 }
