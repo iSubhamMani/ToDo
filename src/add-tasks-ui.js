0742a37a085266accd00f71f802c4cfb5633639e
Subham Mani <manisubham09@gmail.com>
2023-09-15 20:26:21 +0530

    retrieve data from db

--- src/add-tasks-ui.js
+++ src/add-tasks-ui.js
@@ -1,3 +1,5 @@
+import getKey from './add-tasks';
+
 export default function createTasksUI(){
     const addTaskContainer = document.createElement('div');
     addTaskContainer.classList.add('add-tasks-container');
@@ -8,4 +10,42 @@ export default function createTasksUI(){
     addTaskContainer.appendChild(addTasksBtn);
 
     return addTaskContainer;
+}
+
+export function addTaskToDOM(){
+    const taskKey = getKey();
+    const task = JSON.parse(localStorage.getItem(taskKey));
+
+    if(task.title === undefined && task.dueDate === undefined) return;
+
+    const taskCard = document.createElement('div');
+    taskCard.classList.add('task-card');
+
+    const contentContainer = document.createElement('div');
+    contentContainer.classList.add('content-container');
+
+    const taskTick = document.createElement('span');
+    taskTick.classList.add('task-tick');
+    taskTick.setAttribute('data-key', taskKey);
+
+    const taskEdit = document.createElement('span');
+    taskEdit.classList.add('task-edit');
+    taskEdit.setAttribute('data-key', taskKey);
+
+    const taskTitle = document.createElement('span');
+    taskTitle.classList.add('task-title');
+    taskTitle.textContent = task.title;
+
+    const taskDueDate = document.createElement('span');
+    taskDueDate.classList.add('task-due-date');
+    taskDueDate.textContent = `Due: ${task.dueDate}`;
+
+    contentContainer.appendChild(taskTitle);
+    contentContainer.appendChild(taskDueDate);
+
+    taskCard.appendChild(taskEdit);
+    taskCard.appendChild(contentContainer);
+    taskCard.appendChild(taskTick);
+
+    return taskCard;
 }
