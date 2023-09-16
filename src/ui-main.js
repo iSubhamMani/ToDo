65372444f89f1aebc71cf700dffb23b3f1a7a72b
Subham Mani <manisubham09@gmail.com>
2023-09-16 22:35:31 +0530

    add edit task

--- src/ui-main.js
+++ src/ui-main.js
@@ -1,18 +1,20 @@
 import createNav from "./nav";
-import { toggleHam } from "./nav";
 import createTasksUI from "./add-tasks-ui";
-import createModal from "./add-task-modal";
 import createTaskContainer from "./task-container";
 import loadTasks from "./load-tasks";
 import deleteTask from "./delete-task";
+import createAddTaskModal from "./add-task-modal";
+import createEditTaskModal from "./edit-task-modal";
+import editTask from "./edit-task";
 
 export default function createUI(){
     const content = document.getElementById('content');
     content.appendChild(createNav());
     content.appendChild(createTasksUI());
-    content.appendChild(createModal());
+    content.appendChild(createAddTaskModal());
+    content.appendChild(createEditTaskModal());
     content.appendChild(createTaskContainer());
     loadTasks();
     deleteTask();
-    toggleHam();
+    editTask();
 }
